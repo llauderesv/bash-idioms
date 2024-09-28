a634d31c12952331514349b099fd7f85d0417f0d
llauderesv <llauderesv@gmail.com>
2024-09-28 12:57:00 +0800

    update script

--- ch05/script.sh
+++ ch05/script.sh
@@ -5,7 +5,6 @@ len=${#message}
 # Variables don't need the dollar sign reference in front of them when they are used inside of double parens.
 
 # echo $max
-
 if (( len > 5 )); then
   echo 'Too long'
 else 
